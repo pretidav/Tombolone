553d90bc5accb6a16d2220f5b35275c0d0ebd50b
David Preti <david.preti@csic.es>
2017-07-03 14:58:08 +0200

    generalized to arbitrary number of rows

--- TOMBOLONE.py
+++ TOMBOLONE.py
@@ -15,7 +15,7 @@ import numpy as np
 class cartella(object):
 
 
-    def __init__(self,rows=3):
+    def __init__(self,rows):
         self.rows=rows
         if not isinstance(self.rows, int):
             raise ValueError('Number of rows has to be an integer!')
@@ -77,9 +77,9 @@ class cartella(object):
         self.scheda = np.zeros((self.rows, self.__columns))
 
         for i in range(0,self.rows):
-            unit_deca=np.sort(np.random.choice(9, self.__columns,replace=False)*10)+np.random.randint(1, 9,size=self.__columns)
-            if(unit_deca[-1]>90):
-                unit_deca[-1]=90
+            unit_deca=np.sort(np.random.choice((self.rows*3), self.__columns,replace=False)*10)+np.random.randint(1, 9,size=self.__columns)
+            if(unit_deca[-1]>(self.rows*30)):
+                unit_deca[-1]=(self.rows*30)
             self.scheda[i]=unit_deca
 
         control=True
@@ -89,17 +89,21 @@ class cartella(object):
             for j in [t for t in zip(occur,count) if t[1]>1]:
                 control=True
                 lenght=len(self.scheda[self.scheda==j[0]])
-                numbo=int((j[0]/10))*10+np.random.randint(1, 9,size=lenght)
-                if((numbo>90).any()):
-                    numbo=80+np.random.randint(1, 10,size=length)
+                numbo=int((j[0]/10))*10+np.random.randint(1, (self.rows*3),size=lenght)
+                if( (numbo>(self.rows*30)).any() ):
+                    numbo=(self.rows*30-10)+np.random.randint(1, 10,size=length)
                 self.scheda[self.scheda==j[0]]=numbo
 
 
     def _lex_fill_cartella(self,ntab=1):
         self.scheda = np.zeros((self.rows, self.__columns))
-        inc=np.array([-1,-1,3,3,7,7])
+        inc=np.zeros(6)
+        for j in range(0,3):
+            inc[2*j]=(self.rows-1)*2*j   -1 
+            inc[2*j+1]=(self.rows-1)*2*j -1
         for i in range(0,self.rows):
             self.scheda[i]= np.asarray(self.__columns*( 2*i + ntab + inc[ntab-1]  )) + np.arange(1, self.__columns+1)
+
 
 
 
@@ -110,14 +114,14 @@ class cartella(object):
 
 class tabellone(cartella):
 
-    def __init__(self):
-        self.__missings = None
-
+    def __init__(self,rows):
+        self.rows=rows
 
     def fill_tabellone(self):
         """
 
-        Tabellone contains all the numbers from 1 to 90 in lexicographic order, i.e.
+        Tabellone contains all the numbers from 1 to (rows*5*6) in lexicographic order, i.e. if rows==3 : 
+
         --------------------------------------------------------------------------------------------
         | 1  		2  		... 	5   | 6		7		...		10 |
         | 11 		12 		... 	15  | 16	17		...		20 |
@@ -141,7 +145,7 @@ class tabellone(cartella):
 
         self.tab=[]
         for i in range(0,6):
-            A=cartella(3)
+            A=cartella(self.rows)   #to be generalized 
             A.lex_fill_cartella(i+1)
             self.tab.append(A.scheda)
 
@@ -160,12 +164,12 @@ class player(tabellone):
 
     """
 
-    def __init__(self,seedplayer):
+    def __init__(self,seedplayer,rows):
         self.__seedplayer=seedplayer
         self.prize=dict.fromkeys(['ambo','terna','quaterna','cinquina','tombola'])
         self.__checklist=[2,3,4,5]
         self.__checkbool=[['ambo',False],['terna',False],['quaterna',False],['cinquina',False],['tombola',False]]
-
+        self.rows=rows
 	if not isinstance(self.__seedplayer, int):
             raise ValueError('Player ID has to be an integer!')
 
@@ -183,14 +187,14 @@ class player(tabellone):
         if(Ncart>0):
             self.collection=[]
             for i in range(0,Ncart):
-                C=cartella(3)#to be generalized
+                C=cartella(self.rows)#to be generalized
                 C.fill_cartella(i+self.__seedplayer)
                 self.collection.append(C.scheda)
         elif(Ncart==0):
             temp=tabellone()
             temp.fill_tabellone()
             self.collection=temp.tab
-        self.__rep=np.zeros([len(self.collection),3])
+        self.__rep=np.zeros([len(self.collection),self.rows])
 
 
 
@@ -201,7 +205,7 @@ class player(tabellone):
         for j in range(0,length):
             if extraction in self.collection[j]:
                 self.__rep[j,np.where(self.collection[j]==extraction)[0]]+=1
-            if(self.__rep[j].sum()==15):
+            if(self.__rep[j].sum()==(self.rows*5)):#to be generalized 
                 self.prize['tombola']=True
                 
         for j in range(0,len(self.__checklist)):
@@ -215,3 +219,41 @@ class player(tabellone):
 
         print(self.__rep)
         print(self.prize)
+
+
+
+
+
+#TODO
+class partita(player):
+
+    """
+    partita is an object which takes as input a list of a list with pairs {(playerID,Ncart)}. 
+    It ends when a player scores tombola, and report a log of prizes for every player.
+    """
+
+    def __init__(self,Nplayer,cartlist,rows):
+        self.__Nplayer=Nplayer
+        if not isinstance(self.__Nplayer, int):
+            raise ValueError('Number of players has to be an integer!')
+        self.__cartlist=cartlist
+        if not isinstance(self.__cartlist,):
+             raise ValueError('list of cartelle is not a list!')
+        if(len(self.__cartlist)!=Nplayer):
+             raise ValueError('list of cartelle has to be have Nplayer length!')
+
+    def start(self,seedplay):
+        return self._start(seedplay)
+
+    def log(self):
+        return self._log()
+        
+###########################################################################################
+###########################################################################################
+
+    def _start(self,seedplay):
+        extractions=np.arange(1,91)
+        np.random.shuffle(extractions) #seed? 
+        for i in range(0,len(extractions)):
+            print i
+
--- test.py
+++ test.py
@@ -2,28 +2,33 @@
 
 from TOMBOLONE import cartella,tabellone,player
 import numpy as np
-#A=cartella(3)
-#A.fill_cartella(1500)
-C=tabellone()
+A=cartella(5)
+A.fill_cartella(1500)
+C=tabellone(5)
 C.fill_tabellone()
 
 #for i in range(1,7):
 #    B.lex_fill_cartella(i)
 #    print B.scheda
 #print "\n\n"
-#print A.scheda
-#print "\n\n"
-#print C.tab[1]
+print A.scheda
+print "\n\n"
+print C.tab[0]
+print C.tab[1]
+print C.tab[2]
+print C.tab[3]
+print C.tab[4]
+print C.tab[5]
 
-david=player(100)    
-david.take_cartella(2) 
-for j in range(0,len(david.collection)):
-    print(david.collection[j])
-for i in range(0,3):
-    for j in range(0,5):
-        num=david.collection[0][i,j]
-        print("test number = ", num)
-        david.check_cartella(num)
+#david=player(100,4)    
+#david.take_cartella(2) 
+#for j in range(0,len(david.collection)):
+#    print(david.collection[j])
+#for i in range(0,3):
+#    for j in range(0,5):
+#        num=david.collection[0][i,j]
+#        print("test number = ", num)
+#        david.check_cartella(num)
 
 
 
